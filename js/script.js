9c2b3ddcf52258386cd2af88513fd30c1c5098a8
Fadl Al-Obaid <you@example.com>
2025-03-17 22:28:13 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,26 +1,35 @@
-let menuIcon=document.querySelector('#menu-icon');
-let navbar=document.querySelector('.navbar');
-let section=document.querySelector('section');
-let navLinks=document.querySelector('header nav a');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let section = document.querySelector("section");
+let navLinks = document.querySelector("header nav a");
 
-window.onscroll=()=>{
-    section.forEach(sec =>{
-        let top =window.scrollY;
-        let offset=sec.offsetTop-150;
-        let height=sec.offsetHeight;
-        let id =sec.getAttribute('id');
-        if(top>=offset && top<offset+height){
+window.onscroll = () => {
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a [href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
 
-            navLinks.forEach(links=>{
-                links.classList.remove('active');
-                document.querySelector('header nav a [href*='+ id +']').classList.add('active')
-        })
-        }
-    })
-}
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
-
-menuIcon.onclick=()=>{
-    menuIcon.classList.toggle('bx-x')
-    navbar.classList.toggle('active')
+function myFuncstion() {
+  Swal.fire({
+    title: "Thanks for visiting",
+    icon: "success",
+    draggable: true,
+    
+  });
 }
